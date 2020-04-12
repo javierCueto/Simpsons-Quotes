81300a73df8d67eabc2e8bcf1664efa33c1d6180
Javier <javelie@outlook.com>
2020-04-12 10:50:24 -0600

    Initial Commit

---
+++ Simpsons Quotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Simpsons Quotes
+//
+//  Created by José Javier Cueto Mejía on 12/04/20.
+//  Copyright © 2020 Pozolx. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
