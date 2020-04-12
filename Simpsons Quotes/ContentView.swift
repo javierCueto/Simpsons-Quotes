560e5f9a883c2439a4c2c4871d7e305854a9bf75
Javier <javelie@outlook.com>
2020-04-12 12:38:53 -0600

    main interface

--- Simpsons Quotes/ContentView.swift
+++ Simpsons Quotes/ContentView.swift
@@ -9,13 +9,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    let screenWidth = UIScreen.main.bounds.size.width
+    let screenHeight = UIScreen.main.bounds.size.height
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack{
+            VStack(){
+                Text("Insert Brain Here!!")
+                    .fontWeight(.medium)
+                    .font(.custom("Chalkboard SE", size: 30))
+                    .foregroundColor(Color.init("GrayGreenColor"))
+            } .frame(maxWidth: .infinity, maxHeight: (screenHeight/3) - 100)
+            
+            //Spacer()
+            
+            VStack(){
+                    Image("mainHomer")
+                     .resizable()
+                     .scaledToFit()
+            } .frame(maxWidth: .infinity, maxHeight: (screenHeight/3) + 100)
+            
+            //Spacer()
+            
+            ZStack(){
+                VStack(){
+                    Spacer()
+                    Rectangle()
+                        .fill(Color.init("BlueColor"))
+                        .frame(maxWidth: .infinity, maxHeight: (screenHeight/3)/2)
+                    
+                }
+                
+                
+                
+                ZStack{
+                    Button(action: {
+                        
+                    }){
+                        VStack(){
+                            Image(systemName: "quote.bubble.fill")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 100, height: 100)
+                                .foregroundColor(.white)
+                            
+                            
+                        } .frame(width: (screenHeight/3)/1.5, height: (screenHeight/3)/1.5)
+                        
+                    }.foregroundColor(.black)
+                        .background(Color.init("YellowColor"))
+                        .clipShape(Circle())
+                        .shadow(color: Color.init("GrayGreenColorT"), radius: 7)
+                }
+                
+                
+                
+                
+            } .frame(maxWidth: .infinity, maxHeight: screenHeight/3)
+            
+            
+        }.edgesIgnoringSafeArea(.all)
+            .background(Color.init("WhiteColor"))
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    
     static var previews: some View {
         ContentView()
     }
 }
+
